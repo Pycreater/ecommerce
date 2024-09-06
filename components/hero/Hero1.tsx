c9bb3b6096bcec1ceafc06e5908fa9afcd0d8a08
Pratik <pratikyadav3949@gmail.com>
2024-09-06 17:32:46 +0530

    feat(type) fix types

--- components/hero/Hero1.tsx
+++ components/hero/Hero1.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { ContainerScroll } from "../ui/container-scroll-animation";
+import Image from "next/image";
 
 const Hero1 = () => {
   return (
     <ContainerScroll titleComponent={"Find It, Love It, Buy It"}>
-      <img
+      <Image
         src="/assets/hero.png"
         alt="hero"
         className="w-full h-full object-cover"
+        width={500}
+        height={300}
       />
     </ContainerScroll>
   );
